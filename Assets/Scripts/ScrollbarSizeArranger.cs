430833aa8019a3b812b277724aed974ee521c5c5
fcakyon <fcakyon@gmail.com>
2018-08-22 15:21:08 +0300

    7 gun onceki haline getirildi

---
+++ Assets/Scripts/ScrollbarSizeArranger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollbarSizeArranger : MonoBehaviour
+{
+
+    public Scrollbar scrollbar;
+
+    void Start()
+    {
+        scrollbar.value = 1;
+    }
+
+    public void ResetList()
+    {
+        scrollbar.value = 1;
+    }
+}
